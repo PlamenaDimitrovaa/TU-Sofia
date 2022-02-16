a8aa685de2a8adab8b5e0bccbca6166340aedb64
Plamena <plamena3456@abv.bg>
2022-02-16 19:46:45 +0200

    Added C exercises.

---
+++ C/maxLine/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 1001
+
+int main()
+{
+   char line[MAXSIZE];
+   char maxLine[MAXSIZE];
+   maxLine[0] = '\0';
+
+   while(fgets(line, MAXSIZE, stdin) != NULL){
+     if(strlen(line) > strlen(maxLine)){
+         strcpy(maxLine, line);
+     }
+   }
+   puts(maxLine);
+   printf("Max line is: %s\nLength: %u\n", maxLine, (unsigned)strlen(maxLine));
+     return 0;
+}
