128038c2af80fab1c081d364f52edf82f6498942
Plamena <plamena3456@abv.bg>
2022-03-11 10:36:26 +0200

    Added C Pointers exercises.

--- C/Pointers_1/main.c
+++ C/Pointers_1/main.c
@@ -7,8 +7,10 @@ int main()
     int secondNumber = 2;
     int *firstAddress;
     int *secondAddress;
+
     firstAddress = &firstNumber;
     secondAddress = &secondNumber;
+
     printf("Result of their sum: %d\n", *firstAddress + *secondAddress);
     printf("Result of their subtraction: %d\n", *firstAddress - *secondAddress);
     printf("Result of their multiplication: %d\n", *firstAddress * *secondAddress);
