2fa45df9373b10888423ed96f5ec27526bf9e490
Plamena <plamena3456@abv.bg>
2024-04-25 19:51:27 +0300

    Added PS Exercises.

---
+++ PS/PS_42_Plamena/Welcome/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using Welcome.Others;
+
+namespace Welcome.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public UserRolesEnum Role { get; set; }
+
+        public string FacultyNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ PS/PS_42_Plamena/WelcomeExtended/Helpers/UserHelper.cs
@@ -0,0 +1,30 @@
+﻿using Welcome.Model;
+using WelcomeExtended.Data;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserHelper
+    {
+        public static void ToString(this User user) { }
+
+        public static void ValidateCredentials(this UserData user, string name, string password)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new Exception("The name cannot be empty");
+            }
+
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new Exception("The password cannot be empty");
+            }
+
+            //TODO: Invoke ValidateUser(string name, string password); from UserData
+        }
+
+        public static void GetUser(this UserData user)
+        {
+            user.GetUser();
+        }
+    }
+}
