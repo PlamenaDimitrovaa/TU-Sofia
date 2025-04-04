608b5376e6795fd8952e948c05a179d79e6b1598
Plamena P. Dimitrova <PPDimitrova@technologica.com>
2025-04-04 17:20:01 +0300

    Added Transliteration project.

---
+++ C#/Transliteration/Transliteration.Data.Modelss/SearchLog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Transliteration.Data.Models
+{
+    public class SearchLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Query { get; set; }
+
+        public DateTime SearchDate { get; set; } = DateTime.UtcNow;
+    }
+}
