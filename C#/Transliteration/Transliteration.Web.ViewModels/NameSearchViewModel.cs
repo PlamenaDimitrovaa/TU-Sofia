c1bc48d13b2c97499114ac87a897be6e9ca74b0f
Plamena P. Dimitrova <PPDimitrova@technologica.com>
2025-04-04 17:30:06 +0300

    Added Transliteration project.

---
+++ C#/Transliteration/Transliteration.Web.ViewModels/NameSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Transliteration.Data.Models;
+
+namespace Transliteration.Web.ViewModels
+{
+    public class NameSearchViewModel
+    {
+        public string SearchQuery { get; set; }
+        public List<NameEntry> Results { get; set; }
+    }
+}
